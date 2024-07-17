c6946dc54841b697db85f17a788b501ec2222e89
BayuDC <dev.bayudc@gmail.com>
2024-07-17 18:23:34 +0700

    feat: secure with basic auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: ['@nuxt/ui', '@pinia/nuxt', 'nuxt-mongoose'],
+  modules: ['@nuxt/ui', '@pinia/nuxt', 'nuxt-mongoose', '@kgierke/nuxt-basic-auth'],
   imports: {
     dirs: ['types/*.ts'],
+  },
+  basicAuth: {
+    enabled: true,
   },
 });
